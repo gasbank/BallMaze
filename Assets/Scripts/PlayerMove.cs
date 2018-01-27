5c464b539d8675a5c27f91ced63043957551f69c
Geoyeob Kim <gasbank@gmail.com>
2018-01-28 02:33:27 +0900

    처음

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float unitForce = 0.5f;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rb.AddForce(Input.GetAxis("Horizontal") * unitForce, 0, Input.GetAxis("Vertical") * unitForce);
+    }
+}
